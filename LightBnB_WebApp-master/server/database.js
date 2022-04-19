a29392d9c7be28f2c68e450e33ef6a1be5c36941
Edgarsar <edgarsargsyan@hotmail.com>
2022-04-19 03:31:55 +0000

    Refactor addUser function to add new user into DB

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -68,10 +68,9 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool.query(
+    "INSERT INTO users (name, email, password) VALUES ($1, $2, $3)",
+    [user.name, user.email, user.password]);
 }
 exports.addUser = addUser;
 
